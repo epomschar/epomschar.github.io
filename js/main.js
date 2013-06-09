15b38cd695e0fe72ea58892a0c7088af97b601ed
epomschar <rokker@seznam.cz>
2013-06-09 14:08:33 +0100

    text truncate

--- js/main.js
+++ js/main.js
@@ -2,7 +2,8 @@ $(document).ready(function () {
 
     var $mainnav = $('.main-nav'),
 		$subnav = $('.dropdown01 ul, .dropdown02 ul'),
-		$dropdown01 = $('.dropdown01');
+		$dropdown01 = $('.dropdown01'),
+        $paragraph = $('article p');
 
     //main nav hover
     $mainnav.children('li').hover(function () {
@@ -20,14 +21,47 @@ $(document).ready(function () {
         $dropdown01.removeClass('bg-color02 color-000');
         $dropdown01.children('span').removeClass('arrow-down-active');
     });
-    
+
     $subnav.addClass('hidden');
-    
+
     $dropdown01.bind('click', function (event) {
-    	event.stopPropagation();
+        event.stopPropagation();
         $(this).find('ul').removeClass('hidden');
         $(this).addClass('bg-color02 color-000').removeClass('color-fff').unbind('mouseenter');
         $(this).children('span').removeClass('arrow-down-hover').addClass('arrow-down-active');
     })
+
+    //text truncate
+    function truncateParagraph(element, textLength) {
+
+        var maxLength = textLength;
+
+        element.each(function () {
+
+            if (element.text().length > maxLength) {
+
+                var short_text = $(this).text().substring(0, maxLength),
+                short_text = short_text.substring(0, Math.min(short_text.length, short_text.lastIndexOf(" "))),
+                long_text = $(this).text().substring(maxLength);
+
+                element.html(short_text +
+                                        "<a href='#' class='more'> [ ... ]</a>" +
+                                        "<span class='long-text hidden'>" + long_text + "</span>");
+            }
+        });
+
+        $('.more').click(function (event) {
+            event.preventDefault();
+            $(this).addClass('hidden').next().removeClass('hidden');
+        });
+    }
+
+    $('.intro').each(function () {
+        truncateParagraph($(this), 146);
+    });
+
+    $paragraph.each(function () {
+        truncateParagraph($(this), 90);
+    });
 
 });
